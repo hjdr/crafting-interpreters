a69420881e54ea47af4c0d30018b57d8b0aa0b3f
hjdr <harry.jdr@gmail.com>
2020-06-15 22:37:58 +0100

    finished chaper 10 - functions

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -154,7 +154,7 @@ export default class Interpreter implements ExprVisitor<LoxLiteral>, StmtVisitor
   }
 
   public visitFuncStmt(stmt: Func) {
-    const func = new LoxFunction(stmt);
+    const func = new LoxFunction(this.environment, stmt);
     this.environment.define(stmt.name.lexeme, func);
   }
 
--- src/lox-function.ts
+++ src/lox-function.ts
@@ -1,21 +1,20 @@
 import { LoxCallable } from './lox-callable';
-import {
-  Func,
-  Return,
-} from './stmt';
+import { Func } from './stmt';
 import { LoxLiteral } from './token';
 import Interpreter from './interpreter';
 import { Environment } from './environment';
 
 export class LoxFunction implements LoxCallable {
   private declaration: Func;
+  private closure: Environment;
 
-  constructor(declaration: Func) {
-    this.declaration = declaration
+  constructor(closure: Environment, declaration: Func) {
+    this.closure = closure;
+    this.declaration = declaration;
   }
 
   public call(interpreter: Interpreter, args: Array<LoxLiteral>): LoxLiteral {
-    const environment = new Environment(interpreter.globals);
+    const environment = new Environment(this.closure);
     for (let i = 0; i < this.declaration.params.length; i +=1 ) {
       environment.define(this.declaration.params[i].lexeme, args[i])
     }
